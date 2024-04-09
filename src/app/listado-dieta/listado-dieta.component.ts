fd705bd640027c640dcefbc5d3eb6eebe6255296
Ana Gil <gilorozcoana@gmail.com>
2024-04-09 13:22:36 +0200

    Listado usuario
    Hay un error cuando se borran todas las listas

---
+++ src/app/listado-dieta/listado-dieta.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+import { Dieta, DietaImpl } from '../entities/dieta';
+import { FormularioDietaComponent } from '../formulario-dieta/formulario-dieta.component';
+import { DietasService } from '../services/dietas.service';
+import { UsuariosService } from '../services/usuarios.service';
+import { Rol } from '../entities/login';
+
+
+@Component({
+  selector: 'app-listado-dieta',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './listado-dieta.component.html',
+  styleUrl: './listado-dieta.component.css'
+})
+export class ListadoDietaComponent {
+  dietas: Dieta [] = [];
+
+  constructor(private dietasService: DietasService, private usuariosService: UsuariosService, private modalService: NgbModal) {
+    this.actualizarDietas();
+  }
+
+  actualizarDietas() {
+    this.dietasService.getDietas().subscribe(dietas => {
+      this.dietas = dietas;
+    });
+  }
+
+  private dietaEditada(dieta: Dieta): void {
+    this.dietasService.editarDietas(dieta).subscribe(() => {
+      this.actualizarDietas();
+    });
+  }
+
+  editarDieta(dieta: Dieta): void {
+    let ref = this.modalService.open(FormularioDietaComponent);
+    ref.componentInstance.accion = "Editar";
+    ref.componentInstance.dieta = {...dieta};
+    ref.result.then((dieta: Dieta) => {
+      this.dietaEditada(dieta);
+    }, (reason) => {});
+  }
+
+  eliminarDieta(id: number): void {
+    this.dietasService.eliminarDieta(id).subscribe(() => {
+      this.actualizarDietas();
+    });
+  }
+
+  aniadirDieta(): void {
+    let ref = this.modalService.open(FormularioDietaComponent);
+    ref.componentInstance.accion = "Añadir";
+    ref.componentInstance.usuario = new DietaImpl();
+    ref.result.then((dieta: Dieta) => {
+      this.dietasService.aniadirDieta(dieta).subscribe(usuario => {
+        this.actualizarDietas();
+      });
+    }, (reason) => {});
+  }
+  
+  // Estas dos funciones las he hecho basandome en listado-usario.component.ts. Creo que en realidad tendría más sentido poner los roles en servicios si lo vamos a usar en más componentes
+  private get rol() {
+    return this.usuariosService.rolCentro;
+  }
+
+  isAdministrador(): boolean {
+    console.log("Pregunta admin: "+this.rol);
+    return this.rol?.rol == Rol.ADMINISTRADOR;
+  }
+}
---
+++ src/app/services/dietas.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { Login, UsuarioSesion, Rol, RolCentro } from "../entities/login";
+import { Observable, of, forkJoin, concatMap, lastValueFrom } from "rxjs";
+import { map } from 'rxjs/operators';
+//import * as jose from 'jose';
+
+import { Dieta } from "../entities/dieta";
+import { BackendFakeService } from "./backend.fake.service";
+import { BackendService } from "./backend.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DietasService {
+
+  constructor(private backend: BackendFakeService) { }
+
+  getDietas(): Observable<Dieta[]> {
+    return this.backend.getDietas();
+  }
+
+  editarDietas(dieta: Dieta): Observable<Dieta> {
+    return this.backend.putDieta(dieta);
+  }
+
+  eliminarDieta(id: number): Observable<void> {
+    return this.backend.deleteDieta(id);
+  }
+
+  aniadirDieta(dieta: Dieta): Observable<Dieta> {
+    return this.backend.postDieta(dieta);
+  }
+}
