549c5c40f1d4fa3bfad960f502d3c45a95701a1b
Ana Gil <gilorozcoana@gmail.com>
2024-05-31 10:16:19 +0200

    He añadido la carpeta para tests (de momento no tiene ninguno)

---
+++ Prueba_Entrega_3/src/test/DietaTests.java
@@ -0,0 +1,109 @@
+package test;
+
+import es.uma.informatica.sii.spring.jpa.demo.entities.Dieta;
+import es.uma.informatica.sii.spring.jpa.demo.repositories.DietaRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
+
+import java.net.URI;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DietaTests {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Value(value = "${local.server.port}")
+    private int port;
+
+    @Autowired
+    private DietaRepository dietaRepo;
+
+    @BeforeEach
+    public void initializeDatabase() {
+        grupoRepository.deleteAll();
+        alumnoRepository.deleteAll();
+    }
+
+    private URI uri(String scheme, String host, int port, String... paths) {
+        UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+        UriBuilder ub = ubf.builder()
+            .scheme(scheme)
+            .host(host).port(port);
+        for (String path : paths) {
+            ub = ub.path(path);
+        }
+        return ub.build();
+    }
+
+    private RequestEntity<Void> get(String scheme, String host, int port, String path) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.get(uri)
+            .accept(MediaType.APPLICATION_JSON)
+            .build();
+        return peticion;
+    }
+
+    private RequestEntity<Void> delete(String scheme, String host, int port, String path) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.delete(uri)
+            .build();
+        return peticion;
+    }
+
+    private <T> RequestEntity<T> post(String scheme, String host, int port, String path, T object) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.post(uri)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(object);
+        return peticion;
+    }
+
+    private <T> RequestEntity<T> put(String scheme, String host, int port, String path, T object) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.put(uri)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(object);
+        return peticion;
+    }
+
+    @Nested 
+    @DisplayName("Cuando no hay ninguna dieta")
+    public class DietasVacías {
+        @Test
+        @DisplayName("devuelve una lista de dietas vacía")
+        public void devuelveDietas() {
+            var peticion = get("http", "localhost", port, "/dieta");
+
+            var respuesta = restTemplate.exchange(peticion,
+                new ParameterizedTypeReference<List<Dieta>>() {
+                });
+
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+            assertThat(respuesta.getBody()).isEmpty();
+        }
+    }
+}
